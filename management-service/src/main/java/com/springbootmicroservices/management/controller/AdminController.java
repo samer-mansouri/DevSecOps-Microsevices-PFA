1cf208c5b71d01eee04844f86d047c50b6cc6f7b
Omar <omar.maalej@insat.ucar.tn>
2025-06-01 16:39:43 +0100

    fix: test ci management-service

--- management-service/src/main/java/com/springbootmicroservices/management/controller/AdminController.java
+++ management-service/src/main/java/com/springbootmicroservices/management/controller/AdminController.java
@@ -30,7 +30,7 @@ public class AdminController {
 
         LOGGER.info("AdminController | createAdvertisement | userId : " + userId);
 
-        LOGGER.info("Test CI/CD 2");
+        LOGGER.info("Test CI/CD");
 
         adminService.createAdvertisement(advertisementRequest,userId);
         return ResponseEntity.status(HttpStatus.CREATED).body("Advertisement Created");
